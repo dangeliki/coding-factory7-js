f484d016c51d6856ff2025385873e0cac39b1611
dangeliki <angeliki.donta@outlook.com>
2025-05-03 17:24:09 +0300

    cf7

---
+++ review/4b-user.js
@@ -0,0 +1,13 @@
+const user = {
+  firstname : "Alice",
+  age: 25,
+  isStudent : false
+}
+
+console.log(user.firstname)
+console.log(user.age)
+console.log(user.isStudent)
+
+console.log(user["firstname"])
+console.log(user["age"])
+console.log(user["isStudent"])
