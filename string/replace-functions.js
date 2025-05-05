5b526aa2debc88a6940d6a9b868dea2f280709be
dangeliki <angeliki.donta@outlook.com>
2025-05-05 10:48:38 +0300

    cf7

---
+++ string/replace-functions.js
@@ -0,0 +1,15 @@
+const s = "Coding Factory"
+const s2 = "CODING Factory"
+
+const replace = s.replace("Coding","Code")
+const lower = s.toLowerCase()
+const upper = s.toUpperCase()
+
+if(s.toUpperCase() === s1.toUpperCase()) {
+  console.log("EQUALS")
+}
+
+const firstname = "Alice        "
+if(firstname.trim() === "Alice") {
+  console.log("EQUAL")
+}
---
+++ string/search.js
@@ -0,0 +1,13 @@
+const s = "Coding Factory"
+
+let position = s.indexOf("d")
+let position2 = s.lastIndexOf("o")
+const char = s.charAt(0)
+
+if (s.includes("F")) {
+  console.log("F Found")
+}
+
+console.log(position)
+console.log(position2)
+console.log(char)
