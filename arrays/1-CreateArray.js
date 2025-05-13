7aaa16d7cef4f287d98ed2f0926f302d904fe04e
dangeliki <angeliki.donta@outlook.com>
2025-05-13 21:03:19 +0300

    cf7

---
+++ arrays/1-CreateArray.js
@@ -0,0 +1,7 @@
+const arr = []                              //κενός πίνακας
+const arr1 = [1, 2, 3, 4, 5]                 //και populate
+const arr2 = [1, "Alice", 3.4, true]
+const sparseArr = [1, ,3]                    //sparse array
+const arr3 = [,,,]                          //length is 3
+const arr4 = Array(2)
+const grid = [[1,2],[3,4],[5,6,7]]
