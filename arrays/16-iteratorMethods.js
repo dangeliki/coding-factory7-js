6f339b289ee149edfb19b47f4c4f98c077e95c8e
dangeliki <angeliki.donta@outlook.com>
2025-05-24 10:31:54 +0300

    cf7

--- arrays/16-iteratorMethods.js
+++ arrays/16-iteratorMethods.js
@@ -9,3 +9,7 @@ console.log(filtered)
 
 let mapped = students.map(student => "Student: " +student)
 console.log(mapped)
+
+const numbers = [1,6,9,12]
+let sum = numbers.reduce((total,val) => total + val , 0)
+console.log(sum)
