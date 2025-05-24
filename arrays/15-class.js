eb5cfd677a384578b9282a5eface05b6ee48999f
dangeliki <angeliki.donta@outlook.com>
2025-05-24 09:04:30 +0300

    class

--- arrays/15-class.js
+++ arrays/15-class.js
@@ -5,3 +5,19 @@ class ProductAlreadyExistsError extends Error {
   }
 }
 
+class ProductNotFoundError extends Error {
+  constructor(message) {
+    super(message)
+    this.code = "ProductNotFound"
+  }
+}
+
+try {
+    throw new ProductNotFoundError("ProductNotFound")
+} catch(error) {
+    if(error instanceof ProductNotFoundError) {
+      console.error("Product not exists. \n" + error.message,error.code)
+    } else if(error instanceof ProductAlreadyExistsError) {
+      console.error("Product already exists. \n" + error.message, error.code)
+    }
+}
