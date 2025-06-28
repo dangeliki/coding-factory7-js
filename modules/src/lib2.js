d31253c70a4bab71f6ff59a5d5fea687bc360651
dangeliki <angeliki.donta@outlook.com>
2025-06-28 10:47:50 +0300

    modules initial commit

---
+++ modules/src/lib2.js
@@ -0,0 +1,10 @@
+// Named exports
+const PI = 3.14
+const EPSILON = 0.000005
+const API_URL = "https://codingfactory.aueb.gr"
+
+// Default Exports 
+const SEC_PER_MINUTE = 60
+export default SEC_PER_MINUTE
+
+export { PI, EPSILON, API_URL}
