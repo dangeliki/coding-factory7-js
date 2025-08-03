642a972c74f3c1a1794124ba11ef027810c353b3
dangeliki <angeliki.donta@outlook.com>
2025-08-03 14:12:31 +0300

    feat: notes app created

--- browser/notes-app/main.js
+++ browser/notes-app/main.js
@@ -40,29 +40,62 @@ function printGRDate() {
   document.querySelector('#dateTxt').innerHTML = dateStr + '<br>' + timeStr
 }
 
-
+  /**
+   * Controller in MVC.
+   * @param {Object} data 
+   * @returns 
+   */
   function onInsertHandler(data) {
     if(!data.note) return
     insertNote(data)
     reset()
   }
 
+  /**
+   * Model function , inserts a new note
+   * @param {Object} note 
+   */
   function insertNote(note) {
     notes = [...notes,note]
     count ++
     renderNotes()
   }
 
+  /**
+   * View rerendering
+   */
   function renderNotes() {
     const container = document.getElementById('notesWrapper')
 
     container.innerHTML = notes.map((note) => `
     <div id="${'noteTemplate' + note.key}" class="flex justify-between items-center px-[10px] py-[2px] border-b border-black">
-    <div id="${'noteInfo' + note.key}" class = "flex items-center">
-      <input type="checkbox" id="${'noteCheck' + note.key}" onclick = "strikeThrough(${note.key})" class="w-[25px] h-[25px] mr-[5px]" ${note.softDeleted ? 'checked' : ""}>
-      <label id="${'noteTxt' + note.key}" for = "${'noteCheck' + note.key}" class = "w-[200px] max-h-[150px] leading-[1.2rem] overflow-hidden break-words whitespace-normal text-base ${note.softDeleted ? 'line-through text-gray-500' : ''}">${note.note}</label>
-    </div>
-    <button type = "button" id= "${'noteDelBtn' + note.key}" class= "w-[35px] h-[35px]" onclick="deleteNote(${note.key})">X</button>
+      <div id="${'noteInfo' + note.key}" class = "flex items-center">
+        <input type="checkbox" id="${'noteCheck' + note.key}" onclick = "strikeThrough(${note.key})" class="w-[25px] h-[25px] mr-[5px]" ${note.softDeleted ? 'checked' : ""}>
+        <label id="${'noteTxt' + note.key}" for = "${'noteCheck' + note.key}" class = "w-[200px] max-h-[150px] leading-[1.2rem] overflow-hidden break-words whitespace-normal text-base ${note.softDeleted ? 'line-through text-gray-500' : ''}">${note.note}</label>
+      </div>
+      <button type = "button" id= "${'noteDelBtn' + note.key}" class= "w-[35px] h-[35px]" onclick="deleteNote(${note.key})">X</button>
+    </div>`).join("")
+  }
 
-    `)
+  /**
+   * Model Functions
+   */
+
+  function strikeThrough(key) {
+    notes = notes.map(note => 
+      note.key === key ? {...note, softDeleted : !note.softDeleted} : {...note}
+    )
+    renderNotes()
+  }
+
+  function deleteNote(key) {
+    notes = notes.filter(note => note.key !== key)
+    renderNotes()
+  }
+
+  /**
+   * View Function
+   */
+  function reset() {
+    document.querySelector('inputNote').value = ""
   }
